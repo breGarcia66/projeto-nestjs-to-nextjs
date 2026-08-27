2c0312bda4e35d91090ce601463607b9438b2eba
brenno_levy <brennol3vy@gmail.com>
2026-08-27 14:42:35 -0300

    Trabalhando com corpo da requisição

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 @Injectable()
 export class AuthService {
-  doLogin() {
-    return 'Olá mundo!!!';
+  doLogin(loginDto: LoginDto) {
+    console.log('Retornando usuário...');
+    return loginDto;
   }
 }
