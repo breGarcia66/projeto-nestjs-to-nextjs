137e28791221b924cfb000cf9124ea389bc8972a
brenno_levy <brennol3vy@gmail.com>
2026-09-01 13:04:06 -0300

    usando typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
-import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -12,6 +13,27 @@ import { ConfigModule } from '@nestjs/config';
     UserModule,
     PostModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        if (process.env.DB_TYPE === 'better-sqlite3') {
+          return {
+            type: 'better-sqlite3',
+            database: process.env.DB_DATABASE,
+            synchronize: process.env.DB_SYNCHRONIZE === '1',
+            autoLoadEntities: process.env.DB_AUTO_RELOAD_ENTITIES === '1'
+          }
+        }
+
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT || '5432', 10),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE
+        }
+      }
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
